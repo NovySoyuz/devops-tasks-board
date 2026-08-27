2ddb5bcab5ac48a162d1c67b2b08b6cdd4ff64a6
fcabassut <florentcabassut@orange.fr>
2026-08-27 10:18:16 +0200

    ajout de SonarCloud analysis

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: [],
+    coverage: {
+      provider: "v8",
+      reporter: ["lcov", "text"],
+      reportsDirectory: "./coverage",
+    },
   },
 })
